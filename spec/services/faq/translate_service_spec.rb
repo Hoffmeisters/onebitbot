693efa60b3de7859e7bbb9bcdc85c83bd3558583
Hoffmeisters <danilo_hoffmeister@hotmail.com>
2020-01-18 17:08:24 -0300

    first complete human commit

--- spec/services/faq/translate_service_spec.rb
+++ spec/services/faq/translate_service_spec.rb
@@ -4,9 +4,9 @@ describe FaqModule::TranslateService do
     describe "#call" do
         context "Valid sentence" do
             it "Return translated sentence" do
-                @translateService = FaqModule::TranslateService.new({'query' => 'oi'})
+                @translateService = FaqModule::TranslateService.new({'query' => 'sapo'})
                 response = @translateService.call()
-                expect(response).to match("hi")
+                expect(response).to match("the frog")
             end
         end
 
