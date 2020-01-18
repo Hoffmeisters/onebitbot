025bddb2afe36ff8f9acffbffff88ec2affdeeb6
Hoffmeisters <danilo_hoffmeister@hotmail.com>
2020-01-18 17:21:03 -0300

    update translate service module

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -6,11 +6,11 @@ module FaqModule
 
         def call            
           #Para testar inserir real valor no local das env vars, já em produção criar as env vars no heroku
+
           translate_api_url = ENV['TRANSLATE_API_URL']
           api_key = ENV['TRANSLATE_API_KEY']
     
           url = "#{translate_api_url}?key=#{api_key}&text=#{@query}&lang=pt-en"
-          puts url
           res = RestClient.get url
     
           value = JSON.parse(res.body)["text"][0]
